12f4191007724498b28e939b1f23648c7d58f51b
zhangxp1998 <zhangxp1998@gmail.com>
2016-11-05 07:49:27 -0400

    create login code

--- BackendServer/Server.py
+++ BackendServer/Server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import os
 import sqlite3
 import re
 from flask import Flask
@@ -8,13 +7,54 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import flash
+from flask_login import *
+import os
+
 DATABASE = './dream.db'
 app = Flask(__name__)
+login_manager = LoginManager()
+
+class User(UserMixin):
+    # proxy for a database of users
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
 
 @app.route('/')
 def go_home():
     return redirect(url_for('handle_search'))
 
+@login_manager.user_loader
+def load_user(user_id):
+    print(user_id)
+    return User(user_id, '')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return send_from_directory('../', 'login.html')
+    username = request.form['user']
+    password = request.form['pwd']
+    registered_user = User(username, password)
+    if(password == 'hackholyoke4'):
+        login_user(registered_user)
+        flash('Logged in successfully')
+        return redirect(url_for('handle_search'))
+    else:
+        flash('Password incorrect!')
+        return redirect(url_for('login'))
+
+@app.route('/protected.html', methods=['GET'])
+@login_required
+def handle_protected():
+    print("Trying to access protected stuff...")
+    return send_from_directory('../', 'protected.html')
+    
 #remember to replace form.html with actual path
 @app.route('/form.html', methods=['POST'])
 def handle_search():
@@ -70,5 +110,6 @@ def catch_all(path):
     return send_from_directory('../', path);
 
 if __name__ == '__main__':
-    os.chdir('.')
+    login_manager.init_app(app)
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', port=80)
